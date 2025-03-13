2f6a85be7721cd0319eec9bd698abdaf53db4401
Hafeezbaig <hafeezbaig3138@gmail.com>
2025-03-13 12:42:50 +0530

    config updated

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,20 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  theme: {
+    extend: {
+      fontFamily: {
+        visualverse: ['visualverse', 'sanf-serif'],
+        general: ['general', 'sanf-serif'],
+        'circular-web': ['circular-web', 'sanf-serif'],
+        'robert-medium': ['robert-medium', 'sanf-serif'],
+        'robert-regular': ['robert-regular', 'sanf-serif'],
+      },
+
+      
+    },
+  },
+
   plugins: [react(),
     tailwindcss(),
 
